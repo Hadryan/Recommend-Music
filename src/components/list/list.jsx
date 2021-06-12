2dccffd7bff04cbb2413f6ad02807f831b97ca99
chaekie <chaekie.kim@gmail.com>
2021-06-12 22:18:21 +0900

    list 컴포넌트 생성 날씨별 플레이리스트 모음

---
+++ src/components/list/list.jsx
@@ -0,0 +1,29 @@
+import React, { Component } from "react";
+
+class List extends Component {
+  render() {
+    return (
+      <div className="music">
+        <span className="list">전체(1,234)</span>
+        <div className="playlist-wrap">
+          <div className="platlist">
+            <img src="img/summer.jpeg" alt="platlist-cover" className="cover" />
+            <div className="info">
+              <span className="title">선선한 초여름의 낭만을 느끼며</span>
+              <div className="category">
+                <span className="filter">#여름</span>
+                <span className="filter">#화창한 날</span>
+              </div>
+              <div className="songs">
+                <i className="fas fa-compact-disc"></i>
+                <span className="count">30곡</span>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default List;
