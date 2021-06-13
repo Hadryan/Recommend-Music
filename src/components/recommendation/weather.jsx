67541815c1cc6a9629badb6db37fe9d9305d2f77
chaekie <chaekie.kim@gmail.com>
2021-06-13 23:27:23 +0900

    weather description 한국어로 설정 현재 날씨에 맞는 아이콘 추가

--- src/components/recommendation/weather.jsx
+++ src/components/recommendation/weather.jsx
@@ -1,11 +1,12 @@
 import React, { useState, useEffect } from "react";
 
 const Weather = () => {
-  const API_KEY = "1f8df0d58280f08cfe4b490e40aceb02";
   const [status, setStatus] = useState(null);
   const [temp, setTemp] = useState(null);
   const [location, setLocation] = useState(null);
   const [state, setState] = useState(null);
+  const [icon, setIcon] = useState(null);
+  const API_KEY = "1f8df0d58280f08cfe4b490e40aceb02";
 
   useEffect(() => {
     const getLocation = () => {
@@ -18,13 +19,14 @@ const Weather = () => {
             setStatus(null);
             const lat = position.coords.latitude;
             const lon = position.coords.longitude;
-            const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
+            const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&lang=kr&units=metric`;
             fetch(url)
               .then((response) => response.json())
               .then((data) => {
                 setTemp(`${Math.round(data.main.temp)}°C`);
                 setLocation(data.name);
                 setState(data.weather[0].description);
+                setIcon(data.weather[0].icon);
               })
               .catch((error) => console.log("error", error));
           },
@@ -39,10 +41,16 @@ const Weather = () => {
 
   return (
     <div className="weather">
-      <i className="fas fa-cloud-rain icon"></i>
+      <p>{status}</p>
+      {icon && (
+        <img
+          className="icon"
+          src={`http://openweathermap.org/img/wn/${icon}@2x.png`}
+          alt="wIcon"
+        />
+      )}
       <div className="info">
         <div className="tempAndLoc">
-          <p>{status}</p>
           {temp && <span className="temperature">{temp}</span>}
           {location && <span className="location">{location}</span>}
         </div>
