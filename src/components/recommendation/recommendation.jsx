2b070e216303891596ee5c54418c04e9659847cd
chaekie <chaekie.kim@gmail.com>
2021-06-15 22:31:39 +0900

    슬라이더 형식(carousel) 삭제 현재 날씨에 맞는 플레이리스트는 하나만 뜨도록 수정

--- src/components/recommendation/recommendation.jsx
+++ src/components/recommendation/recommendation.jsx
@@ -7,30 +7,18 @@ class Recommendation extends Component {
     return (
       <div className="recommendation-warp">
         <span className="title">비오는 날 추천 음악</span>
-        <div className="carousel-wrap">
-          <button className="btn left">
-            <i className="fas fa-chevron-left"></i>
-          </button>
-          <button className="btn right">
-            <i className="fas fa-chevron-right"></i>
-          </button>
-          <div className="recommendations">
-            <div className="recommendation">
-              <Weather />
-              <div className="playlist">
-                <div className="music">
-                  <img
-                    src="img/yerinbaek_ep.jpeg"
-                    alt="album-cover"
-                    className="album-cover"
-                  />
-                  <div className="song-info">
-                    <span className="name">
-                      그건 아마 우리의 잘못은 아닐거야
-                    </span>
-                    <span className="artist">백예린</span>
-                  </div>
-                </div>
+        <div className="recommendation">
+          <Weather />
+          <div className="playlist">
+            <div className="music">
+              <img
+                src="img/yerinbaek_ep.jpeg"
+                alt="album-cover"
+                className="album-cover"
+              />
+              <div className="song-info">
+                <span className="name">그건 아마 우리의 잘못은 아닐거야</span>
+                <span className="artist">백예린</span>
               </div>
             </div>
           </div>
